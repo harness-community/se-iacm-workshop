701d3fe351ed9956847584729e5b75e5d7c13166
Tom Kouhsari <thomas.kouhsari@harness.io>
2025-10-31 14:53:15 -0400

    AMI change

--- aws/v1/resources.tf
+++ aws/v1/resources.tf
@@ -21,6 +21,7 @@ resource "aws_subnet" "main" {
 resource "aws_instance" "ec2-be" {
   instance_type = var.instance_type
   ami           = "ami-04e5276ebb8451442"
+  //ami           = "ami-080c353f4798a202f"
   count         = 3
 
   subnet_id = aws_subnet.main.id
